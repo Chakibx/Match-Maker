dcc84671b7c0c01c4afcf3ad132655df4f355616
chakibx <chakib_moussaoui@outlook.fr>
2023-04-01 15:36:17 +0200

    salut

--- main.c
+++ main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#define TAILLE_TABLEAU 100
+char * tableau[TAILLE_TABLEAU];
+
+int readCmd() {
+    int nombreDeMots = 0;
+    char ligne[TAILLE_TABLEAU];
+
+
+    printf("$ ");
+    scanf("%[^\n]", ligne);
+
+    tableau[nombreDeMots] = strtok(ligne, " ");
+
+    while ( tableau[nombreDeMots] != NULL ) {
+        nombreDeMots ++;
+        tableau[nombreDeMots] = strtok ( NULL, " " );
+    }
+    return nombreDeMots;
+}
+
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
-    return 1;
+    int n = readCmd();
+    while ( n>0) {
+        pid_t pid;
+        pid = fork();
+        if (pid=0){
+            execvp(tableau[0],tableau);
+        } else {
+            printf("Erreur lors de la création du processus fils \n");
+        }
+    }
 }

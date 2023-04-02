c7b9b0bd411ee24e0c27200af29879a36dc9bff6
Aymen Zemmouri <zemmouriaymene@gmail.com>
2023-04-02 16:42:47 +0200

    V1

--- shuffle.c
+++ shuffle.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
 #define MAX_TEAMS 32
 #define DURATION 90 // default match duration is 90 minutes, equivalent to 5400sec
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #define MAX_TEAM_NAME_LEN 50
-#define MAX_NUM_TEAMS 100
+#define MAX_NUM_TEAMS 32
 void shuffle(char teams[][MAX_TEAM_NAME_LEN], int n);
 
 char** read_team_names(char* filename, int* num_teams, int* match_duration) {
     FILE *file;
-    char **team_names = (char**) malloc(MAX_NUM_TEAMS * sizeof(char*));
+    char **team_name = (char**) malloc(MAX_NUM_TEAMS * sizeof(char*));
     char line[MAX_TEAM_NAME_LEN];
 
     for (int i = 0; i < MAX_NUM_TEAMS; i++) {
-        team_names[i] = (char*) malloc(MAX_TEAM_NAME_LEN * sizeof(char));
+        team_name[i] = (char*) malloc(MAX_TEAM_NAME_LEN * sizeof(char));
     }
 
     file = fopen(filename, "r");
+    printf("filename '%s'\n",filename);
     if (file == NULL) {
         printf("Erreur lors de l'ouverture du fichier.");
         exit(1);
@@ -29,6 +26,7 @@ char** read_team_names(char* filename, int* num_teams, int* match_duration) {
 
     // Lecture de la durée du match sur la première ligne
     fgets(line, sizeof(line), file);
+    //fscanf(file,"DUREE=%d",*match_duration);
     *match_duration = atoi(line);
 
     // Lecture des noms d'équipes sur les lignes suivantes
@@ -37,7 +35,7 @@ char** read_team_names(char* filename, int* num_teams, int* match_duration) {
         line[strcspn(line, "\n")] = 0;
 
         // Ajoute le nom de l'équipe à la liste
-        strcpy(team_names[*num_teams], line);
+        strcpy(team_name[*num_teams], line);
         (*num_teams)++;
     }
 
@@ -45,9 +43,9 @@ char** read_team_names(char* filename, int* num_teams, int* match_duration) {
 
     // Mélange les noms d'équipes de manière aléatoire
     srand(time(NULL));
-    shuffle(team_names, *num_teams);
+    shuffle(team_name, *num_teams);
 
-    return team_names;
+    return team_name;
 }
 
 void shuffle(char teams[][MAX_TEAM_NAME_LEN], int n) {

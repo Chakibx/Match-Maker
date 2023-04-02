e5f6a4e3576ef3f3d7725a30184b1d8436ed4412
ayoub <ayoubsalah0111@gmail.com>
2023-04-02 15:49:01 +0200

    lecture fichier liste chainnee

--- lecturefichier.c
+++ lecturefichier.c
@@ -312,6 +312,8 @@ int main() {
     return 0;
 }
 */
+/*
+//version correcte mais n'ignore pas la durree lors du remplissage des equipes
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -339,6 +341,82 @@ int main() {
     // Lecture de la durée du match
     fscanf(fp, "%d", &duration);
     printf("Durée du match : %d minutes\n", duration);
+
+    // Lecture des équipes
+    while(fgets(ligne, sizeof(ligne), fp) != NULL) {
+        // Enlever le saut de ligne à la fin de la ligne
+        ligne[strcspn(ligne, "\n")] = 0;
+        // Créer une nouvelle équipe
+        char* nom = strdup(ligne);
+        // Créer un nouveau nœud pour l'équipe
+        noeud* n = (noeud*) malloc(sizeof(noeud));
+        n->nom = nom;
+        n->suivant = NULL;
+
+        // Lier le nouveau nœud au nœud précédent de la liste chaînée
+        if(tete == NULL) {
+            tete = n;
+            dernier = n;
+        }
+        else {
+            dernier->suivant = n;
+            dernier = n;
+        }
+    }
+
+    fclose(fp);
+
+    // Parcourir la liste chaînée et afficher les équipes
+    noeud* courant = tete;
+    while(courant != NULL) {
+        printf("Equipe : %s*\n", courant->nom);
+        courant = courant->suivant;
+    }
+
+    // Libérer la mémoire allouée pour les nœuds et les noms d'équipe
+    courant = tete;
+    while(courant != NULL) {
+        noeud* suivant = courant->suivant;
+        free(courant->nom);
+        free(courant);
+        courant = suivant;
+    }
+
+    return 0;
+}
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Structure pour représenter un nœud de la liste chaînée
+typedef struct noeud {
+    char* nom;
+    struct noeud* suivant;
+} noeud;
+
+int main() {
+    FILE *fp;
+    int duration;
+    char ligne[100];
+    char* equipe_nom;
+    noeud* tete = NULL;
+    noeud* dernier = NULL;
+
+    fp = fopen("/home/ayoub/university/equipe.txt", "r");
+    if(fp == NULL) {
+        printf("Erreur d'ouverture du fichier.");
+        return 1;
+    }
+
+    // Lecture de la durée du match
+    fscanf(fp, "%d", &duration);
+    printf("Durée du match : %d minutes\n", duration);
+
+    // Ignorer la première ligne (caractère de fin de ligne)
+    fgets(ligne, sizeof(ligne), fp);
 
     // Lecture des équipes
     while(fgets(ligne, sizeof(ligne), fp) != NULL) {

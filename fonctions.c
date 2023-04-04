3575985af137e81ed62ce0021f95b9e5d6e1c458
chakibx <chakib_moussaoui@outlook.fr>
2023-04-04 10:57:35 +0200

    V4

--- fonctions.c
+++ fonctions.c
@@ -1,5 +1,14 @@
 #include "fonctions.h"
 
+/**
+*@brief Lit les noms d'équipe à partir d'un fichier et les stocke dans un tableau de chaînes alloué dynamiquement.
+*@param filename Le nom du fichier contenant les noms des équipes.
+*@param num_teams Un pointeur vers un entier qui stockera le nombre d'équipes lues à partir du fichier.
+*@param team_names Un pointeur vers un pointeur qui stockera le tableau des noms d'équipe.
+*@pre Le paramètre filename doit être un chemin d'accès valide vers un fichier contenant des noms d'équipe, et les paramètres num_teams et team_names doivent être non nuls.
+*@post Le paramètre num_teams sera mis à jour avec le nombre d'équipes lues à partir du fichier, et le paramètre team_names pointera vers un tableau de chaînes alloué dynamiquement contenant les noms des équipes.
+*@return vide.
+*/
 void read_team_names(char* filename, int* num_teams, char *** team_names) {
     FILE *file;
     *team_names = (char**) malloc(MAX_TEAMS * sizeof(char*));
@@ -53,7 +62,6 @@ void read_team_names(char* filename, int* num_teams, char *** team_names) {
             (*num_teams)++;
         }
     }
-    printf("num_teams %d\n",(*num_teams));
     if (((*num_teams) & ((*num_teams) - 1)) != 0 || (*num_teams) > MAX_TEAMS)
     {
         printf("Number of teams must be a power of 2 and less than %d\n", MAX_TEAMS);
@@ -73,6 +81,14 @@ void read_team_names(char* filename, int* num_teams, char *** team_names) {
         strcpy((*team_names)[j], temp);
     }
 }
+/**
+*@brief La fonction simule un match pour le mode "Simulation concurrente", cette fonction s'execute dans un thread, ce qui permet d'executer plusieurs matchs en meme temps avec un mecanisme de verrouillage avec mutex pour eviter les problemes liées aux acces concurrents.
+* Elle simule le match en générant des scores aléatoires pour chaque équipe en utilisant la fonction rand().
+* Le match est simulé pendant un certain temps défini par la constante match_duration, et si les scores sont égaux à la fin du temps réglementaire, une séance de tirs au but est effectuée pour déterminer le vainqueur.
+* Si une équipe gagne le match, la fonction met à jour le tableau teams_remaining qui indique quelles équipes sont encore en compétition et à quel tour. Elle utilise également un verrou (mutex) pour éviter les conflits d'accès au tableau par plusieurs threads en même temps.
+*@param ma Pointeur qui est ensuite casté en une structure Match. Corresspond au match qui va etre simulé
+*@return void*
+*/
 void *simulate_match(void *ma)
 {
     Match match = (Match) ma;
@@ -80,13 +96,13 @@ void *simulate_match(void *ma)
     int duration = 0;
     int action;
 
-    // Simulate match by randomly generating scores
+    // Debut de la simulation
     printf("DEBUT %s %d - %d %s [TOUR %d]\n",team_names[match->team1],match->score1, match->score2,team_names[match->team2],match->tour);
     while (duration < match_duration)
     {
-        action = rand() % 100; // simulate random action
+        action = rand() % 100; //Simule une action aleatoire
         if (action < 98)
-        { // 98% de chance de ne pas marquer
+        { // 98% de chance de ne pas marquer pour les deux equipes
             duration++;
         }
         else if (action == 99 )
@@ -101,10 +117,11 @@ void *simulate_match(void *ma)
             duration++;
             printf("(%d') %.20s %d - %d %-20s\n",duration, team_names[match->team1], match->score1, match->score2, team_names[match->team2]);
         }
-        sleep(0.005);
+        sleep(1); //mettre en pause pendant 1 seconde
     }
+    //Si le score reste nul, alors execution de la séance de tirs au buts
     int nTab = 5;
-    while(match->score1 == match->score2){ //SI MATCH NUL, SEANCE DE TAB
+    while(match->score1 == match->score2){
         int tab;
         while(nTab > 0){
             tab = rand() % 100;
@@ -118,31 +135,37 @@ void *simulate_match(void *ma)
                 printf("%.20s (%d) - (%d) %-20s\n", team_names[match->team1], match->score1, match->score2, team_names[match->team2]);
             }
             nTab--;
-        }
+        } //Si apres les 5 TAB, le score reste vierge, alors chacune des deux equipes tir une fois, la premiere qui rate perd le match
         nTab=1;
     }
-    if(match->score1 > match->score2){
-        pthread_mutex_lock(&mutex);
-        teams_remaining[match->team1] = match->tour+1;
-        teams_remaining[match->team2] = -1;
-        pthread_mutex_unlock(&mutex);
-        printf("FIN %s* %d - %d %s\n",team_names[match->team1], match->score1, match->score2, team_names[match->team2]);
+    if(match->score1 > match->score2){ // Si l'équipe 1 a gagné
+        pthread_mutex_lock(&mutex); // Verrouillage du mutex pour accéder à la variable partagée
+        teams_remaining[match->team1] = match->tour+1; // On met à jour le tableau des équipes restantes en compétition
+        teams_remaining[match->team2] = -1; // On indique que l'équipe 2 est éliminée
+        pthread_mutex_unlock(&mutex); // Déverrouillage du mutex
+        printf("FIN %s* %d - %d %s\n",team_names[match->team1], match->score1, match->score2, team_names[match->team2]); // On affiche le résultat du match avec une astérisque à côté du nom de l'équipe gagnante
     }
-    else{
-        pthread_mutex_lock(&mutex);
-        teams_remaining[match->team2] = match->tour+1;
-        teams_remaining[match->team1] = -1;
-        pthread_mutex_unlock(&mutex);
-        printf("FIN %s %d - %d %s*\n",team_names[match->team1], match->score1, match->score2, team_names[match->team2]);
+    else{ // Si l'équipe 2 a gagné ou s'il y a match nul
+        pthread_mutex_lock(&mutex); // Verrouillage du mutex pour accéder à la variable partagée
+        teams_remaining[match->team2] = match->tour+1; // On met à jour le tableau des équipes restantes en compétition
+        teams_remaining[match->team1] = -1; // On indique que l'équipe 1 est éliminée
+        pthread_mutex_unlock(&mutex); // Déverrouillage du mutex
+        printf("FIN %s %d - %d %s*\n",team_names[match->team1], match->score1, match->score2, team_names[match->team2]); // On affiche le résultat du match avec une astérisque à côté du nom de l'équipe gagnante
     }
+
     pthread_exit(0);
 }
-void play_match(Match match){ //fonction qui permet de
-    int res = -1;
-    int valid = 0;
-    int sleep = 1000000; //duree entre
+/**
+*@brief Simule un match pour le mode manuel, la fonction propose à l'utilisateur de choisir entre : [1] Simuler le deroulement du match en temps reel minute par minute, avec possibilité d'interruption en cliquant sur une touche afin de choisir quelle equipe marque [2]: Choisir directement un score pour le match
+*@param ma Pointeur sur la structure Match
+*@return void*
+*/
+void play_match(Match match){
+    int valid = 0; //choix valide ou pas
+    int sleep = 1000000; //temps d'arret a chaque boucle, en microsecondes
     int action;
-    int mode ; //Choisir entre soit de simuler un match, deroulement en temps reel avec possibilite d'interruption OU choisir un score
+    int mode ; //Choix du mode de simulation manuel
+    int res = -1;
 
     printf("Match %s VS %s [TOUR %d]\n",team_names[match->team1],team_names[match->team2],match->tour);
     do {
@@ -150,7 +173,7 @@ void play_match(Match match){ //fonction qui permet de
         scanf("%d",&mode);
     }while(mode != 1 && mode != 2);
 
-    if(mode==1){
+    if(mode==1){ //Mode "Simuler"
         printf("DEBUT %s %d - %d %s [TOUR %d]\n",team_names[match->team1],match->score1, match->score2,team_names[match->team2],match->tour);
         for (int duration = 0; duration <= 90; duration++) {
             usleep(sleep);
@@ -171,14 +194,14 @@ void play_match(Match match){ //fonction qui permet de
                     while ((c = getchar()) != '\n' && c != EOF) {}
                     scanf("%d",&res);
                     if(res==0){
-                        sleep = 0;
+                        sleep = 0; //accelerer le match en mettant le temps d'arret a chaque boucle a 0
                         valid = 1;
-                    }else if(res==1){
+                    }else if(res==1){ //l'equipe 1 marque
                         match->score1++;
                         printf("(%d') %.20s %d - %d %-20s\n",duration, team_names[match->team1], match->score1, match->score2,
                                team_names[match->team2]);
                         valid = 1;
-                    }else if(res==2){
+                    }else if(res==2){//l'equipe 2 marque
                         match->score2++;
                         printf("(%d') %.20s %d - %d %-20s\n",duration, team_names[match->team1], match->score1, match->score2,
                                team_names[match->team2]);
@@ -186,27 +209,24 @@ void play_match(Match match){ //fonction qui permet de
                     }else{printf("Veuillez choisir entre 0, 1 et 2\n");}
                 }
             }else{
-                action = rand() % 100; // simulate random action
+                action = rand() % 100; // Simule une action aleatoire
                 if (action < 98) { // 98% de chance de ne pas marquer
-                    //duration++;
                     printf("(%d')\n", duration);
                 } else if (action == 99) { // 1% de chance de marquer pour l'equipe 1
                     match->score1++;
-                    //duration++;
                     printf("(%d') %.20s %d - %d %-20s\n", duration, team_names[match->team1], match->score1, match->score2,
                            team_names[match->team2]);
                 } else //action == 100
                 { // 1% de chance de marquer pour l'equipe 2
                     match->score2++;
-                    //duration++;
                     printf("(%d') %.20s %d - %d %-20s\n", duration, team_names[match->team1], match->score1, match->score2,
                            team_names[match->team2]);
                 }
             }
         }
-
+        //Si le score reste nul, alors execution de la séance de tirs au buts
         int nTab = 5;
-        while (match->score1 == match->score2) { //SI MATCH NUL, SEANCE DE TAB
+        while (match->score1 == match->score2) {
             int tab;
             while (nTab > 0) {
                 tab = rand() % 100;
@@ -223,20 +243,17 @@ void play_match(Match match){ //fonction qui permet de
                 }
                 nTab--;
             }
-            nTab = 1;
+            nTab = 1; //Si apres les 5 TAB, le score reste vierge, alors chacune des deux equipes tir une fois, la premiere qui rate perd le match
         }
     }
 
-    if(mode==2){
+    if(mode==2){ //Mode "Choisir un score"
         int score1 = 0, score2 = 0;
         do {
-            fflush(stdin);
             printf("Score %s: ",team_names[match->team1]);
             scanf("%d",&score1);
-            fflush(stdin);
             printf("Score %s: ",team_names[match->team2]);
             scanf("%d",&score2);
-            fflush(stdin);
             if(score1 == score2){
                 printf("Veuillez choisir un score non nul \n");
             }
@@ -256,6 +273,18 @@ void play_match(Match match){ //fonction qui permet de
         printf("FIN %s %d - %d %s*\n",team_names[match->team1], match->score1, match->score2, team_names[match->team2]);
     }
 }
+/**
+ *@brief Enregistre les informations des matchs dans un fichier texte.
+ Cette fonction ouvre un fichier texte en mode écriture, puis écrit les informations
+ de chaque match dans le fichier sous la forme suivante :
+ "Match [numéro] : [nom de l'équipe 1] ([score de l'équipe 1]) : ([score de l'équipe 2]) [nom de l'équipe 2] | Tour [numéro du tour]"
+ Les informations sont extraites du tableau "matchs" et du tableau "team_names".
+ Le fichier est ensuite fermé avant la fin de la fonction.
+ *@param team_names Le tableau de noms des équipes.
+ *@param matchs Le tableau de matchs à enregistrer.
+ *@param num_match Le nombre de matchs à enregistrer.
+ *@return void
+*/
 void save_matchs(char **team_names, Match *matchs, int num_match) {
     // Ouverture du fichier en mode écriture
     FILE *fp = fopen("matchs.txt", "w");
@@ -269,14 +298,20 @@ void save_matchs(char **team_names, Match *matchs, int num_match) {
         fprintf(fp, "Match %d : %s [%d] : [%d] %s | Tour %d\n", i + 1, team_names[matchs[i]->team1], matchs[i]->score1, matchs[i]->score2, team_names[matchs[i]->team2], matchs[i]->tour);
     }
 
-    fclose(fp); // Fermeture du fichier
+    fclose(fp); //Fermeture du fichier
 }
+/**
+@brief Libérez toute la mémoire allouée dynamiquement utilisée dans le programme.
+Cette fonction libère la mémoire utilisée pour les noms d'équipe et le tableau des équipes restantes dans le tournoi.
+@retour vide
+*/
 void free_memory() {
+    //Liberation du tableau des noms des equipes
     for (int i = 0; i < num_teams; i++) {
         free(team_names[i]);
     }
     free(team_names);
+    //Liberation du tableau des equipes restantes
     free(teams_remaining);
 }
-
 
--- main.c
+++ main.c
@@ -1,9 +1,50 @@
 #include "fonctions.h"
+/**
+ * @file main.c
+ * @brief Programme principal pour la simulation du tournoi
+ * Ce programme simule un tournoi entre plusieurs équipes de façon concurrente ou manuelle.
+ * Les equipes ainsi que la duree du match sont lus à partir d'un fichier passé en argument
+ * Le résultat des matchs est enregistré dans un fichier de sortie.
+ * @author Ayoub SALAH
+ * @author Chakib MOUSSAOUI
+ * @author Aymen ZEMMOURI
+ * @date 2023
+ * @version 1.0
+*/
+
+/**
+ *@brief Durée des matchs en secondes
+*/
 int match_duration = DURATION;
+
+/**
+ *@brief Nombre d'équipes participant au tournoi
+*/
 int num_teams;
+
+/**
+ *@brief Tableau des noms des équipes, indicé par les numeros des equipes
+*/
 char **team_names;
+
+/**
+ *@brief Tableau des équipes restantes, indicé par les numeros des equipes, contient (-1: si l'equipe est eliminé | 0: si l'equipe est entrain de jouer | n: si l'equipe est au tour n, prete a jouer)
+*/
 int * teams_remaining;
+
+/**
+ *@brief Mutex utilisé pour la synchronisation des threads dans le mode "Simulation concurrente"
+*/
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+
+/**
+ *@brief Fonction principale
+ *Cette fonction lit le nom des équipes depuis un fichier, initialise les tableaux et les structures nécessaires pour la simulation, et lance la simulation du tournoi en mode concurrent ou manuel.
+ *@param argc Nombre d'arguments passés au programme
+ *@param argv Tableau de chaînes de caractères contenant les arguments passés au programme
+ *@return Toujours 1
+*/
 int main(int argc, char *argv[])
 {
     char * filename;
@@ -14,12 +55,12 @@ int main(int argc, char *argv[])
     }
 
     //Creation d'une liste randomise avec les equipes
-    //Fin lecture des equipes
     read_team_names(filename, &num_teams, &team_names);
 
     //Allocation du tableau teams_remaining
     teams_remaining = (int *)malloc(num_teams*sizeof(int));
-    //Initialisation du tableau a 1 qui corresspond au numero du tour
+
+    //Initialisation du tableau a 1, toutes les equipes sont au debut au tour 1
     for (int i = 0; i < num_teams; i++)
     {
         teams_remaining[i] = 1;
@@ -31,36 +72,36 @@ int main(int argc, char *argv[])
     int team1 = 0;
     int team2 = 0;
     int tour = 1;
-    int manual=1 ;
+    int manual = 1;
     do {
         printf("Choisir le mode de jeu : [1]:Mode simulation concurrente | [2]:Mode manuel \n");
         scanf("%d",&manual);
     }while(manual != 1 && manual != 2);
 
-    //******************************************************************************************
-    if (manual == 1) {
-
+    if (manual == 1) { //Mode "Simulation concurrente"
         pthread_mutex_init(&mutex,NULL);
 
-        // Create threads to play matches
-        pthread_t threads[num_matchs_total];
+        pthread_t threads[num_matchs_total]; //Tableau de threads de taille egale au nombre de match totale
         while(num_match < num_matchs_total){
             for (team1 = 0; team1 < num_teams; team1++)
             {
-                pthread_mutex_lock(&mutex);
-                if(teams_remaining[team1] > 0){
-                    tour = teams_remaining[team1]; //le tour
+                pthread_mutex_lock(&mutex); //Verrouiler le mutex avant d'acceder au tableau teams_ramining
+                if(teams_remaining[team1] > 0){ //L'equipe team1 est prete a jouer le tour=teams_remaining[team1]
+                    tour = teams_remaining[team1];
                     team2 = team1+1;
-                    while(team2 < num_teams){
+                    while(team2 < num_teams){ //Rrouver l'equipe 2 qui a le meme numero de tour que team1
                         if(teams_remaining[team2] == tour){
+                            //Creaion d'un match entre team1 et team2
                             matchs[num_match] = (Match)malloc(sizeof(struct Match));
                             matchs[num_match]->team1 = team1;
                             matchs[num_match]->team2 = team2;
                             matchs[num_match]->score1 = 0;
                             matchs[num_match]->score2 = 0;
                             matchs[num_match]->tour = teams_remaining[team2];
+                            //Mettre a 0 les cases de teams_remaining d'indices team1 et team2, signifie qu'ils sont en train de jouer un match
                             teams_remaining[team1] = 0;
                             teams_remaining[team2] = 0;
+                            //Creation d'un thread pour ce match
                             pthread_create(&threads[num_match], NULL, simulate_match, matchs[num_match]);
                             num_match++;
                             team2++;
@@ -69,7 +110,7 @@ int main(int argc, char *argv[])
                         team2++;
                     }
                 }
-                pthread_mutex_unlock(&mutex);
+                pthread_mutex_unlock(&mutex);//Deverouillage du mutex
             }
         }
 
@@ -78,7 +119,7 @@ int main(int argc, char *argv[])
             pthread_join(threads[i], NULL);
         }
         pthread_mutex_destroy(&mutex);
-    }else {//***********************************************************************************
+    }else { //Mode Manuel
         while(num_match<num_matchs_total) {
             team1=0;
             team2=0;
